bb2847374b54cb13ff15118d61944c72c94c9e16
ItsThanos1 <157194187+ItsThanos1@users.noreply.github.com>
2024-04-22 16:30:36 -0600

    final final update 3

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -8,6 +8,7 @@ import { Image } from 'react-bootstrap';
 class MovieDetail extends Component {
   constructor(props) {
     super(props);
+    this.updateReview=this.updateReview.bind(this)
     this.state = {
       review: {
         username: localStorage.getItem('username'),
