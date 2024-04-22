fe4b89cd1018f87c5b7f0b8c8fcbde3d990b152c
ItsThanos1 <157194187+ItsThanos1@users.noreply.github.com>
2024-04-22 17:48:02 -0600

    final final update 8

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -20,9 +20,7 @@ class MovieDetail extends Component {
 
   componentDidMount() {
     const { dispatch } = this.props;
-    if (this.props.selectedMovie == null) {
-      dispatch(fetchMovie(this.props.movieId));
-    }
+    dispatch(fetchMovie(this.props.movieId));
   }
 
   
@@ -57,74 +55,77 @@ class MovieDetail extends Component {
   };
 
   render() {
-    
-      return (
-        <Card>
-          <Card.Header>Movie Detail</Card.Header>
-          <Card.Body>
-            <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
-          </Card.Body>
-          <ListGroup>
-            <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
-            <ListGroupItem>
-              {this.props.selectedMovie.actors.map((actor, i) => (
-                <p key={i}>
-                  <b>{actor.actorName}</b> {actor.characterName}
-                </p>
-              ))}
-            </ListGroupItem>
-            <ListGroupItem>
-              <h4>
-                <BsStarFill /> {this.props.selectedMovie.avgRating}
-              </h4>
-            </ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            {this.props.selectedMovie.movieReviews &&
-              this.props.selectedMovie.movieReviews.map((review, i) => (
-                <p key={i}>
-                  <b>{review.username}</b>&nbsp; {review.review} &nbsp; <BsStarFill /> {review.rating}
-                </p>
-              ))}
-          </Card.Body>
-          <Card.Body>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group controlId="reviewRating">
-                <Form.Label>Rating</Form.Label>
-                <Form.Control
-                  as="select"
-                  name="rating"
-                  value={this.state.review.rating}
-                  onChange={this.handleInputChange}
-                >
-                  <option value="0">Select Rating</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="reviewText">
-                <Form.Label>Review</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  name="review"
-                  value={this.state.review.review}
-                  onChange={this.handleInputChange}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit Review
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      )
-    
-
-   
+    const { selectedMovie } = this.props;
+  
+    if (!selectedMovie) {
+      return <div>Loading...</div>;
+    }
+  
+    return (
+      <Card>
+        <Card.Header>Movie Detail</Card.Header>
+        <Card.Body>
+          <Image className="image" src={selectedMovie.imageUrl} thumbnail />
+        </Card.Body>
+        <ListGroup>
+          <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+          <ListGroupItem>
+            {selectedMovie.actors.map((actor, i) => (
+              <p key={i}>
+                <b>{actor.actorName}</b> {actor.characterName}
+              </p>
+            ))}
+          </ListGroupItem>
+          <ListGroupItem>
+            <h4>
+              <BsStarFill /> {selectedMovie.avgRating}
+            </h4>
+          </ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          {selectedMovie.reviews &&
+            selectedMovie.reviews.map((review, i) => (
+              <p key={i}>
+                <b>{review.username}</b>&nbsp; {review.review} &nbsp;
+                <BsStarFill /> {review.rating}
+              </p>
+            ))}
+        </Card.Body>
+        <Card.Body>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="reviewRating">
+              <Form.Label>Rating</Form.Label>
+              <Form.Control
+                as="select"
+                name="rating"
+                value={this.state.review.rating}
+                onChange={this.handleInputChange}
+              >
+                <option value="0">Select Rating</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="reviewText">
+              <Form.Label>Review</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="review"
+                value={this.state.review.review}
+                onChange={this.handleInputChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit Review
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
